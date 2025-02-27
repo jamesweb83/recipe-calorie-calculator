b65ada1444981132d5c7214dd1bf290186436c76
jamesweb83 <pmhdacc@gmail.com>
2025-02-27 11:27:26 +0900

    sdf

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,6 +10,13 @@ function MyApp({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
+        
+        {/* 구글 애드센스 코드 */}
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7988179761724460"
+          crossOrigin="anonymous"
+        />
       </Head>
       <Component {...pageProps} />
     </LanguageProvider>
