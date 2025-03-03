cb61d9bed2f4fcd36e061da25b77304dee1f6ccf
jamesweb83 <pmhdacc@gmail.com>
2025-03-03 12:06:14 +0900

    m

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@
 import { LanguageProvider } from '../contexts/LanguageContext';
 import '../styles/globals.css';
 import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -25,6 +26,25 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
+
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZX3Q4336PS"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZX3Q4336PS');
+          `,
+        }}
+      />
+      
       <Component {...pageProps} />
     </LanguageProvider>
   );
