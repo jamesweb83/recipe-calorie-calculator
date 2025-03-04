1e2b36335bbeeaa4d8f87fecdb37c178fc3efd65
jamesweb83 <pmhdacc@gmail.com>
2025-03-04 10:10:35 +0900

    m

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -15,5 +15,25 @@ module.exports = {
     priority: 0.7,
     sitemapSize: 50000,
     exclude: ['/404'],
-    generateIndexSitemap: false
+    generateIndexSitemap: false,
+    // 각 페이지별 우선순위 설정
+    transform: async (config, path) => {
+      const priorities = {
+        '/': 1.0,
+        '/blog': 0.8,
+        '/blog/accurate-calorie-counting': 0.7,
+        '/blog/low-calorie-dinner-recipes': 0.7,
+        '/blog/korean-food-on-diet': 0.7,
+        '/blog/asian-vs-western-diet': 0.7,
+        '/faq': 0.6,
+        '/privacy-policy': 0.3
+      };
+
+      return {
+        loc: path,
+        changefreq: config.changefreq,
+        priority: priorities[path] || config.priority,
+        lastmod: new Date().toISOString(),
+      };
+    }
   } 
